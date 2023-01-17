3487aac957ac37d26cae0c92a6504ed52bfdfc61
Ethan Wong <87055387+ethanwongca@users.noreply.github.com>
2023-01-16 23:17:04 -0500

    Add files via upload

---
+++ ActiveBuzzer.ino
@@ -0,0 +1,22 @@
+//Active Buzzer Project
+
+ int buzzer = 12;  
+  digitalWrite(buzzer,HIGH);
+    delay(0);
+    digitalWrite(buzzer,LOW);
+    delay(0);
+    }
+    //output another frequency
+     for(i=0;i<100;i++)
+      {
+        digitalWrite(buzzer,HIGH);
+        delay(0);
+        digitalWrite(buzzer,LOW);
+        delay(0);
+      }
+  }
+} 
+
+
+
+
