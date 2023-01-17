06c6a298b888319eda864677311b725722665ad2
Ethan Wong <87055387+ethanwongca@users.noreply.github.com>
2023-01-16 23:15:36 -0500

    Add files via upload

---
+++ PassiveBuzzer.ino
@@ -0,0 +1,15 @@
+//Passive Buzzer Project
+
+int melody[] = {
+  NOTE_D4, NOTE_E4, NOTE_E4, NOTE_D4, NOTE_B3, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_C4,};
+     
+    delay(700);
+  }
+   
+
+  delay(200);
+}
+
+
+
+
